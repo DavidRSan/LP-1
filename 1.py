fddd60c984e021d37be061443e51547a32670624
DavidRSan <134338462+DavidRSan@users.noreply.github.com>
2023-06-06 21:10:24 -0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -132,12 +132,3 @@ programa = [
 
 heap = MemoryHeap(50)
 heap.execute_program(programa)  # Executa o programa
-
-'''
-Resumo:
-O programa simula a alocação e desalocação de blocos de memória em um heap. A classe `FreeBlock` representa um bloco livre no heap, com um endereço inicial e um comprimento. A classe `MemoryHeap` possui métodos para alocar memória usando as estratégias "First Fit" e "Best Fit", além de liberar memória.
-
-O programa lê uma lista de comandos no heap, que incluem a alocação usando as estratégias "First Fit" e "Best Fit" e a desalocação de blocos. Após cada operação, o estado atual do heap é impresso, mostrando os blocos ocupados e livres.
-
-O programa é executado usando um heap com um tamanho específico. Os blocos são alocados no heap de acordo com a estratégia escolhida e os blocos livres são atualizados e mesclados conforme necessário. Os blocos são liberados definindo os valores correspondentes no heap como livres. O programa mostra o estado do heap após cada operação, permitindo visualizar as mudanças na alocação de memória.
-'''
